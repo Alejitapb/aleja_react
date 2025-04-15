12a816f5d4f8fe8842c3911b8459b0bf21edb7a1
Alejandra Pabón Barbosa <alejandra_pabonb@soy.sena.edu.co>
2025-04-15 09:14:56 -0500

    Defino MiComponente.js
    Signed-off-by: Alejandra Pabón Barbosa <alejandra_pabonb@soy.sena.edu.co>

--- src/MiComponente.js
+++ src/MiComponente.js
@@ -1,1 +1,12 @@
-hola
+import React from 'react';
+
+function MiComponente() {
+    return (
+        <div className="mi-componente">
+            <h2>Este es mi primer componente</h2>
+            <p>¡Estoy aprendiendo React!</p>
+        </div>
+    );
+}
+
+export default MiComponente;
